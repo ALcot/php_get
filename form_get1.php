2327fb4d7f032a726f62249dfe7b0867769ec522
ALcot <80142145+ALcot@users.noreply.github.com>
2021-03-15 19:17:52 +0900

    Add files via upload

---
+++ form_get1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>フォーム:GET</title>
+</head>
+<body>
+    
+    <form method="get"action="form_get2.php">
+    <p>お名前:<input type="text" name="name"size="20"</p>
+    <p>MAIL:<input type="text" name="mail" size="20"</p>
+    <p>年齢:<input type="text" name="nenrei" size="20"</p>
+    <p>要望:<input type="text" name="youbou" size="20"</p>
+    <p>住所:<input type="text" name="ad" size="20"</p>
+    <p><input type="submit" value"送信"></p>
+    </form>
+
+</body>
+</html>
---
+++ form_get2.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$nenrei = $_GET["nenrei"];
+$youbou = $_GET["youbou"];
+$ad = $_GET["ad"];
+echo $name."<br>";
+echo $mail."<br>";
+echo $nenrei."<br>";
+echo $youbou."<br>";
+echo $ad."<br>";
+
+?>
